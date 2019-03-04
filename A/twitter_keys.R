afc8c06d6da54846ee03b6b2555a9aa969402c26
Alexander Bieniek <BieniekAlexander@gmail.com>
2019-03-04 16:10:59 +0100

    Delete twitter_keys.R

--- A/twitter_keys.R
+++
@@ -1,5 +0,0 @@
-# twitter keys for @alexbeanyak
-consumer_key = "h1t97lsccXoeY09bm2rWRhYuE"
-consumer_secret = "qEC9wt8Bcqw1FNtgl5sl4wsjF8QWtAzpQF2TCPLOTDxFeE8cPW"
-access_token = "1099684575129882624-DrpgY2yvzSlgxbei5LCo4gxbNZIYPp"
-access_secret = "VoADwBNpE09NlSnKmFCqlCMqRpPDdvwfRRk4eMDLVps38"
